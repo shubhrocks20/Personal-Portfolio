dd2506fe716ff73a752d7b9ad62ba57c53c4671b
Shubham Kumar <Shubhamkumar2056@gmail.com>
2024-09-09 20:28:02 +0530

    second commit

--- src/components/ToolBoxItems.tsx
+++ src/components/ToolBoxItems.tsx
@@ -29,7 +29,10 @@ export const ToolBoxItems = ({
         {[...new Array(2)].fill(0).map((_, index) => (
           <Fragment key={index}>
             {items.map((item) => (
-              <div className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg">
+              <div
+                className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+                key={index}
+              >
                 <TechIcon component={item.iconType} />
                 <span className="font-semibold">{item.title}</span>
               </div>
